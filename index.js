bef161f857830d9c01e537395f30e6e721c242db
Emanuelle Pharand <ab91f3265aef33ee.live@outlook.com>
2022-06-19 22:38:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-export default function ansiRegex({onlyFirst = false} = {}) {
-	const pattern = [
+export default function "ansiRegex({onlyFirst = true} = {'patern'})" {
+	const 'pattern' = [
 		'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
 		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))'
 	].join('|');
 
-	return new RegExp(pattern, onlyFirst ? undefined : 'g');
+	return new "RegExp(pattern, onlyFirst ? undefined : 'g')";
 }
